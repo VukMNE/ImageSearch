fa7f940a83ba0986c7978df15465f84f3a7fa394
Vuk Đuranović <vuk.djuranovic@udg.edu.me>
2018-01-09 18:33:54 +0100

    Search enabled, but without offset

---
+++ program.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var mongo = require('mongodb').MongoClient;
+var cors = require("cors");
+var url =`mongodb://${process.env.USERNAME}:${process.env.PASSWOURD}@ds125914.mlab.com:25914/urls`;
+var apiKey = process.env.APIKEY;
+var idEngine = process.env.ENGINEID;
+
+var GoogleSearch = require('google-search');
+var googleSearch = new GoogleSearch({
+  key: apiKey,
+  cx: idEngine
+});
+var parametri = {
+	query: "",
+	offset: 0
+}
+var app = express();
+app.use(cors());
+app.get("/search/*", function(request, response){
+	parametri.query = request.url.substring(request.url.lastIndexOf("/")+1).split("?")[0];
+	var subster = request.url.split("?")[1];
+	parametri.offset = subster.split("=")[1];
+	
+	googleSearch.build({
+	  q: parametri.query,
+	  start: 0,
+	  fileType: "png%2C+jpg",
+	  num: 10, // Number of search results to return between 1 and 10, inclusive 
+	}, function(error, searchResult) {
+	  console.log(searchResult);
+	  response.send(searchResult);
+	});
+});
+
+app.listen(process.env.PORT || 5000);
+
